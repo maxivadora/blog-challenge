bed372a541f35ce94d44b2d70407fc10c69f2762
Maxi Vadora <maxivadora@gmail.com>
2022-06-24 19:37:31 -0300

    add index and show page

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Post Management', type: :request do
+  let(:post) { create(:post) }
+
   it 'shows a list of posts' do
     get '/posts'
     expect(response).to have_http_status(:ok)
+  end
+
+  it 'shows the post detail' do
+    get "/posts/#{post.id}"
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include(post.title)
   end
 end
